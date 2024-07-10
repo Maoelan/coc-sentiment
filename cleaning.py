dfaae4014ab3f13d1cc23f33f001f0483e9e1ded
Maoelan <maulanamuhammad2000@gmail.com>
2024-07-10 14:23:52 +0700

    documentation cleaning & training

--- cleaning.py
+++ cleaning.py
@@ -2,21 +2,19 @@ import re
 import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from nltk.stem import PorterStemmer
 
-# Function untuk membersihkan teks review dengan penghapusan stopwords dan stemming
 def preprocessing_text(text):
     def cleaning_text(text):
-        text = re.sub(r'@\w+', '', text)  # Hapus mention
-        text = re.sub(r'#\w+', '', text)  # Hapus hashtag
-        text = re.sub(r'RT[\s]', '', text)  # Menghapus RT
-        text = re.sub(r'https?://\S+|www.\S+', '', text)  # Hapus URL
-        text = re.sub(r'<.*?>', '', text)  # Hapus tag HTML
-        text = re.sub(r'\d+', '', text)  # Hapus angka
-        text = text.lower()  # Ubah teks menjadi lowercase
-        text = re.sub(r'[^\w\s]', '', text)  # Menghapus karakter selain huruf dan angka
-        text = text.replace('\n', ' ')  # Mengganti baris baru dengan spasi
+        text = re.sub(r'@\w+', '', text)
+        text = re.sub(r'#\w+', '', text)
+        text = re.sub(r'RT[\s]', '', text)
+        text = re.sub(r'https?://\S+|www.\S+', '', text)
+        text = re.sub(r'<.*?>', '', text)
+        text = re.sub(r'\d+', '', text)
+        text = text.lower()
+        text = re.sub(r'[^\w\s]', '', text)
+        text = text.replace('\n', ' ')
         text = text.translate(str.maketrans('', '', string.punctuation))
         text = text.strip()
         return text
@@ -30,11 +28,6 @@ def preprocessing_text(text):
     def filtering_text(tokens):
         list_stopwords = set(stopwords.words('english'))
         return [word for word in tokens if word.lower() not in list_stopwords]
-    
-    '''def stemming_text(tokens):
-        factory = StemmerFactory()
-        stemmer = factory.create_stemmer()
-        return ' '.join([stemmer.stem(word) for word in tokens])'''
     
     def stem_text(tokens):
         stemmer = PorterStemmer()
